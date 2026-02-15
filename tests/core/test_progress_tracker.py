07bdff9b78ae5814e0b09b9c8ce0b3d2645c7f96
Gerald Lonlas <g.lonlas@gmail.com>
2026-02-15 11:29:20 +0800

    test(core): scaffold test_progress_tracker

---
+++ tests/core/test_progress_tracker.py
@@ -0,0 +1,10 @@
+from src.core.progress_tracker import ProgressTracker
+
+def test_update_before_start_is_noop():
+    pass
+
+def test_progress_context_updates_counts():
+    pass
+
+def test_stop_without_start_is_safe():
+    pass
