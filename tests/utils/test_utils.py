2f593b579864fb00b6e87cba6a4aecb2b49cd29d
Gerald Lonlas <g.lonlas@gmail.com>
2026-02-15 11:29:20 +0800

    test(utils): scaffold test_utils

---
+++ tests/utils/test_utils.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pytest
+from src.utils.path_utils import PathUtils
+from src.utils.size_formatter import SizeFormatter
+
+def test_get_destination_path_changes_extension(tmp_path: Path):
+    pass
+
+def test_get_destination_path_preserves_extension(tmp_path: Path):
+    pass
+
+def test_ensure_parent_dir_creates(tmp_path: Path):
+    pass
+
+def test_ensure_parent_dir_raises_when_parent_is_file(tmp_path: Path):
+    pass
+
+def test_format_size_bytes_and_petabytes():
+    pass
