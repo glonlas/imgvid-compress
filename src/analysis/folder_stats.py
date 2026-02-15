35e5238b6153f5f2e06fee4d0ab09113c3850807
Gerald Lonlas <g.lonlas@gmail.com>
2026-02-15 11:29:11 +0800

    feat(analysis): implement folder_stats logic

--- src/analysis/folder_stats.py
+++ src/analysis/folder_stats.py
@@ -1,11 +1,17 @@
 """Folder stats data container."""
+
 from dataclasses import dataclass
+
 from ..utils.size_formatter import SizeFormatter
+
 
 @dataclass
 class FolderStats:
     """Statistics about a folder."""
 
+    size_bytes: int
+    file_count: int
+
     @property
     def formatted_size(self) -> str:
-        raise NotImplementedError()
+        return SizeFormatter.format_size(self.size_bytes)
